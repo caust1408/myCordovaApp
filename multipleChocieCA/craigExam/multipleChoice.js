bcddbed138201cb8efa73f6162b7e8e71abfb2bd
Craig <cav9871@saintjoe.edu>
2015-03-25 20:20:16 -0400

    This is my cordova app and I must now move my files from www/ file into the cordova one so that I can run it with cordova

---
+++ multipleChocieCA/craigExam/multipleChoice.js
@@ -0,0 +1,12 @@
+var MULTIPLE = [
+  {text: "First state to legalize gay marriage", answer: "Massachusetts", decoys: ["California", "Oregon", "New York", "Indiana", "Mississippi" ] },
+  {text: "The last state to join the union", answer: "Hawaii", decoys: ["Alaska", "Arizona", "Kansas" ] },
+  {text: "Harry Truman\'s home state", answer: "Missouri", decoys: ["Illinois", "Kansas", "Indiana", "Ohio", "Kentucky" ] },
+  {text: "Largest state by area", answer: "Alaska", decoys: ["Texas", "Montana", "Rhode Island" ] },
+  {text: "John Glenn's home state", answer: "Ohio", decoys: ["Pennsylvania", "Arizona", "Florida" ] },
+  {text: "State where Detroit Pistons first played", answer: "Indiana", decoys: ["Michigan", "Minnesota", "Ohio" ] },
+  {text: "The first state to join the union", answer: "Delaware", decoys: ["Georgia", "Massachusetts", "Rhode Island" ] }
+];
+
+if (typeof module != "undefined" && module.exports)
+  module.exports = MULTIPLE;
